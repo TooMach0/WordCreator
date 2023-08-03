3a7a6953c86a2408c7f286d5470e15b53d3ccbd9
TooMach0 <donimichal@gmail.com>
2023-08-03 16:02:05 +0200

    Add project files.

---
+++ WordCreator/Generator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCreator
+{
+    internal class Generator
+    {
+        static string Input(string pismena)
+        {
+            return null;
+        }
+    }
+}
---
+++ WordCreator/ZoznamSlov.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCreator
+{
+    internal class ZoznamSlov
+    {
+        static string[] Slova()
+        {
+            string[] Moznosti = new string[] { "auto", "plzen" };
+
+            return Moznosti;
+        }
+    }
+}
